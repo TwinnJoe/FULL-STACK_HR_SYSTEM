d5726e916c57a84d12cf5a64bd9f7c4c178c29da
Twinn Joe <sinovuyojoe@gmail.com>
2025-04-16 14:07:21 +0200

    Finalize dependency updates

--- app.js
+++ app.js
@@ -24,3 +24,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
